efa93204257558166444d0262a28cf1bb8980a35
Alejandro Yevenes <aeyevenes@gmail.com>
2021-09-13 14:19:47 -0300

    servicios_2

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,27 +1,47 @@
 class ServicesController < ApplicationController
+  before_action only: %i[new edit update destroy]
+
   def index
     @services = Service.all
+  end
+
+  def show
   end
 
   def new
     @service = Service.new
   end
 
-  def show
+  def create
+    @service = Service.new # (service_params)
+    @service.save
+    redirect_to services_path
+  end
+
+  def edit
     @service = Service.find(params[:id])
   end
 
-  def create
-    @service = Service.new(service_params)
+  def update
+    @service = Service.find(params[:id])
+    redirect_to service_path
+    flash[:notice] = 'service was updated.'
   end
 
   def destroy
     @service.destroy
+    redirect_to services_path
   end
 
-  private
+  def show
+    @service = Service.find(params[:id])
+  end
+
+  # def set_service
+  # @service = Service.find(params[:service_id])
+  # end
 
   def service_params
-    params.require(:service).permit(:comment, :service_id)
+    params.require(:service).permit(:name, :address, :telephone, :description)
   end
 end
